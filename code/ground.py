d28ec8441356729ab7e856c210dd76cf6e870149
Edvaldo Mantovanelli <ecmantovanelli@gmail.com>
2019-12-15 20:01:25 -0300

    fix ground init

--- code/ground.py
+++ code/ground.py
@@ -5,8 +5,8 @@ from config import *
 
 
 class Ground(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self, width, height)
+    def __init__(self, width, height):
+        pygame.sprite.Sprite.__init__(self)
 
         self.image = pygame.image.load('../assets/sprites/base.png')
         self.image = pygame.transform.scale(self.image, (width, height))
