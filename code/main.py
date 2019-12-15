51d620004d73bbeaa3cce9ca5abd34e22569a36d
Edvaldo Mantovanelli <ecmantovanelli@gmail.com>
2019-12-15 18:24:22 -0300

    start pygame

---
+++ code/main.py
@@ -0,0 +1,16 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 800
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+
+    pygame.display.update()
