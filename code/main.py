590ad14c73e7672bbc1621ee45c9002d9f234432
Edvaldo Mantovanelli <ecmantovanelli@gmail.com>
2019-12-15 18:39:30 -0300

    adding background

--- code/main.py
+++ code/main.py
@@ -8,9 +8,14 @@ SCREEN_HEIGHT = 800
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+BACKGROUND = pygame.image.load('../assets/sprites/background-day.png')
+BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
+
+    screen.blit(BACKGROUND, (0, 0))
 
     pygame.display.update()
